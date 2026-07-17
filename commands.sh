fc33ed326f4caa6f4f349076001159d4973bc756
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-07-17 16:50:12 +0530

    Backup of logs script

--- commands.sh
+++ commands.sh
@@ -6,3 +6,7 @@ find . -name "*.log"
 
 #Find all the names starting with .log files older than 14 days
 find . -name "*.log" -type f -mtime +14
+
+
+#create a file for required time and date
+touch -d 20260106 filename.txt
