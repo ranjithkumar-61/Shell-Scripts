9103a02aca67fd540d6b96292096521814b57deb
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-07-18 14:51:42 +0530

    Scripts

--- disc_usage.sh
+++ disc_usage.sh
@@ -26,4 +26,4 @@ done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
 
-sh mail.sh "abhishekreddyan@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh "ranjithreddy612002@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
